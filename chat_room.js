609f4072d0dc7c856823552d51a77e70581287fa
Shadow7621 <134629711+Shadow7621@users.noreply.github.com>
2023-07-20 19:02:15 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFDO8Bj7t0HQgP9PcznnabLPC7SZcxI2U",
+    authDomain: "kwitter-acd4d.firebaseapp.com",
+    databaseURL: "https://kwitter-acd4d-default-rtdb.firebaseio.com",
+    projectId: "kwitter-acd4d",
+    storageBucket: "kwitter-acd4d.appspot.com",
+    messagingSenderId: "90369847352",
+    appId: "1:90369847352:web:08009c6050d00ca275b6cd",
+    measurementId: "G-X01BVZQ4RL"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
